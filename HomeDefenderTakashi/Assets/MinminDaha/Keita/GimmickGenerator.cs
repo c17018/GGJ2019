286faa678a3e0f828f15fd8c5a3e1c1913c44ec3
Keta Tamanaha <e185755@ie.u-ryukyu.ac.jp>
2019-01-26 18:32:19 +0900

    キューブ関係の動きを作成

---
+++ HomeDefenderTakashi/Assets/MinminDaha/Keita/GimmickGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GimmickGenerator : MonoBehaviour {
+	[SerializeField] GameObject wall ;
+		
+	public void gimmicgenerator(){
+        Vector3 mousePointInScreen = Input.mousePosition;
+            mousePointInScreen.z+=10.0f;
+            
+            
+            
+		
+		Vector3 mousePointInWorld = Camera.main.ScreenToWorldPoint(mousePointInScreen);
+		
+		Vector3 spawnPosition = new Vector3(mousePointInWorld.x, 0.5f, mousePointInWorld.z);
+		
+		
+		Instantiate(wall, spawnPosition,Quaternion.identity);
+		
+	}
+}
