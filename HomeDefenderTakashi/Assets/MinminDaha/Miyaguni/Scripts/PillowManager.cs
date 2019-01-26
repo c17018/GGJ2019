d638cf2ccd080a7dbda670b6daa5203dacf0409e
c17018 <c17018@std.it-college.ac.jp>
2019-01-26 18:25:38 +0900

    テキスト更新処理の追加、コメントの追加

--- HomeDefenderTakashi/Assets/MinminDaha/Miyaguni/Scripts/PillowManager.cs
+++ HomeDefenderTakashi/Assets/MinminDaha/Miyaguni/Scripts/PillowManager.cs
@@ -8,12 +8,14 @@ public class PillowManager : MonoBehaviour
 
     private IEnumerator Start()
     {
+        // 数秒後にタグを変更(生成したときにPlayerに当たると消えるから、消えないようにするために最初はタグを付けてない)
         yield return new WaitForSeconds(TagWaitTime);
         gameObject.tag = "Pillow";
     }
 
     private void Update()
     {
+        // ステージ外に出て座標が-10より低くなると消える
         if (transform.position.y < -10)
         {
             Destroy(this.gameObject);
--- HomeDefenderTakashi/Assets/MinminDaha/Miyaguni/Scripts/PlayerController.cs
+++ HomeDefenderTakashi/Assets/MinminDaha/Miyaguni/Scripts/PlayerController.cs
@@ -29,14 +29,18 @@ public class PlayerController : MonoBehaviour
 
     private void Start()
     {
-        pillowCount = 3;
-        pillowCountText.text = pillowCount.ToString();
+        // 枕の初期数を定義
+        pillowCount = initPillowCount;
+        // 枕残数テキストを更新
+        pillowCountText.text = "まくらの数: " + pillowCount.ToString();
     }
 
     private void Update()
     {
+        // 動きの更新
         Move();
 
+        // クリックしたときに枕を飛ばす処理を読み込む
         if(Input.GetMouseButtonDown(0) && pillowCount > 0)
         {
             throwPillow();
@@ -46,9 +50,9 @@ public class PlayerController : MonoBehaviour
     // 移動処理
     private void Move()
     {
-        // 方向キーの入力値とカメラの向きから、移動方向を決定
+        // 方向キーの入力で、移動方向を決定
         Vector3 moveForward = Vector3.forward * GetMoveDirection().z + Vector3.right * GetMoveDirection().x;
-        // 移動方向にスピードを掛ける。ジャンプや落下がある場合は、別途Y軸方向の速度ベクトルを足す。
+        // 移動方向にスピードを掛ける
         playerRB.velocity += (moveForward * playerSpeed);
 
         // キャラクターの向きを進行方向に
@@ -61,6 +65,7 @@ public class PlayerController : MonoBehaviour
     // 移動方向取得
     public Vector3 GetMoveDirection()
     {
+        // 入力受け取り
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -69,22 +74,28 @@ public class PlayerController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
+        // 枕回収処理
         if(other.gameObject.tag == "Pillow")
         {
-            pillowCount++;
+            //落ちてる枕を消す
             Destroy(other.gameObject);
-            Debug.Log(pillowCount);
-            pillowCountText.text = pillowCount.ToString();
+            // 枕残数の更新
+            pillowCount++;
+            pillowCountText.text = "まくらの数: " + pillowCount.ToString();
         }
     }
 
+    // 枕を投げる処理
     private void throwPillow()
     {
+        // 枕を生成
         GameObject pillow = Instantiate(playerPillow, transform.position + pillowInstantiatePosision, Quaternion.identity);
+        // 生成した枕の物理演算を参照
         Rigidbody pillowRB = pillow.GetComponent<Rigidbody>();
+        // 枕を前方に飛ばす
         pillowRB.AddForce(transform.forward * pillowSpeed, ForceMode.VelocityChange);
+        // 枕残数を更新
         pillowCount--;
-        Debug.Log(pillowCount);
-        pillowCountText.text = pillowCount.ToString();
+        pillowCountText.text = "まくらの数: " + pillowCount.ToString();
     }
 }
