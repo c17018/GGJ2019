49fd2b47c3d3e499a1129ee8a9dbb8dca752fd08
inamura <e165704@ie.u-ryukyu.ac.jp>
2019-01-26 23:05:07 +0900

    マクラ購入シーンの追加

---
+++ HomeDefenderTakashi/Assets/MinminDaha/inaya/Scripts/GoStage.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoStage : MonoBehaviour
+{
+    [SerializeField] private string SceneName;
+    public void OnClick()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
